64098a0482e96e9f0ea7d0fa6f06f3c6a0fee0ed
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-01-29 09:26:06 +0100

    Add project files.

---
+++ Galeotti_EsercizioGara/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Galeotti_EsercizioGara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quanti sono i partecipanti? ");
+            int p = int.Parse(Console.ReadLine());
+            List<string> partecipanti = new List<string>();
+            for (int i = 1; i <= p; i++)
+            {
+                Console.WriteLine("Nome del partecipante: ");
+                string nome = Console.ReadLine();
+                partecipanti.Add(nome);
+                Thread t = new Thread(() => Corri(nome));
+                t.Start();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Corri(string nome)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine($"Il partecipante {nome} è al km {i + 1}.");
+            }
+            Console.WriteLine($"Il partecipante {nome} è arrivato al km 100 e ha terminato la corsa.");
+        }
+    }
+}
